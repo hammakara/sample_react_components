93fdcd2a75bc8d31c4c0c3dac96f933746c08178
HamMakara <hammakara51@gmail.com>
2026-06-08 16:18:13 +0700

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
 const Footer = () => {
+  const hour = new Date().getHours()
+  const openHour = 7
+  const closeHour = 20
+  const isOpen = (hour >= openHour) && (hour <= closeHour)
+  console.log(isOpen)
+
   return (
     <div className="footer">
-      <p>ចុះឈ្មោះ និងចាប់ផ្តើមរៀនថ្ងៃនេះ</p>
-
-      <button>ទិញ</button>
+      {isOpen ? (
+        <div>
+          <p>ទំលាក់លេខ ទំលាក់ទីតាំង</p>
+          <button>ទិញ</button>
+        </div>
+      ) :
+        (
+          <div>
+            <p>ហាងយើងខ្ញំុ​ដំណើរការនៅចន្លោះម៉ោង {openHour}:00 ដល់ម៉ោង { closeHour}:00</p>
+          </div>
+        )
+      }
     </div>
   )
 }

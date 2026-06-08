992a03600dd32553805cfe2fc8b8321cded023c9
HamMakara <hammakara51@gmail.com>
2026-06-08 19:33:10 +0700

    ready update course array

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import CourseCard from "./components/CourseCard";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 function App() {
+
   return (
     <div className="container">
       <Header />
       <div className="courses">
-        {courses.map((course) => (
-          <CourseCard
-            key={course.id}
-            course={course}
-          />
-        ))}
+        {
+          courses.length > 0 ? courses.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          )) : <div>
+              <h1>No data</h1>
+          </div>
+        }
       </div>
       <Footer />
     </div>
